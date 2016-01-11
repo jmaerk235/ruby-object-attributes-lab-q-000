385aab751367d844a038f69203334df019fa5e2c
jmaerk235 <cmerrick@mail.fresnostate.edu>
2016-01-10 16:55:09 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name=(dog)
+    @name = dog
+  end
+
+  def name
+    @name
+  end
+  
+  def breed=(type)
+    @breed = type
+  end
+
+  def breed
+    @breed
+  end
+
+end 
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+  #def initialize(person)
+  #  @name = person
+  #end
+
+  def name=(person)
+    @name = person
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(employment)
+    @job = employment
+  end
+
+  def job
+    @job
+  end
+
+end
